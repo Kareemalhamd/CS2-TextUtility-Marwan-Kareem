fa0c2dc86128e15b5e1b6c1bd863861eece5cdad
Marwan-AB <Marwan-Aboelnour@aucegypt.edu>
2025-06-25 20:39:19 +0300

    added body of last 4 functions

--- text-utility.cpp
+++ text-utility.cpp
@@ -1,7 +1,7 @@
 #include "text_utility.h"
 #include <cctype> 
 #include <cstring> 
-
+using namespace std;
 
 int wordCount(const char* str) {
     if (!str) return 0;
@@ -60,4 +60,45 @@ void reverseString(char* str) {
         str[len - 1 - i] = temp;
     }
 }
-
+bool palindrome(const char* str){
+if(!str) return false;
+int length = 0;
+while(str[length]!='\0') length++;
+for(int i=0;i<length/2;i++){
+if(str[i]!=str[length-1-i]) return false;
+}
+return true;
+}
+void convert_to_upper(char* str){
+if(!str) return;
+int i =0;
+while(str[i]!='\0'){
+str[i] = toupper(str[i]);
+i++;
+}
+}
+void convert_to_lower(char* str){
+if(!str) return;
+int i =0;
+while(str[i]!='\0'){
+str[i] = tolower(str[i]);
+i++;
+}
+}
+int substringcount(const char* str,const char* substr){
+if(!str || !substr) return 0;
+int len_str = strlen(str);
+int len_sub = strlen(substr);
+if (len_str == 0 || len_sub == 0 || len_sub > len_str) return 0;
+int count=0;
+ for (int i = 0; i <= len_str - len_sub; i++) {
+        int j = 0;
+        while (j < len_sub && str[i + j] == substr[j]) {
+            j++;
+        }
+        if (j == len_sub) {
+            count++;
+        }
+}
+return count;
+}
