a9da5103c60de1242182b93b5a844e6fd378f89f
Kareemalhamd <Kareemalhamd@aucegypt.edu>
2025-06-25 18:03:09 +0300

    Adding functions to text-utility.h

---
+++ text-utility.h
@@ -0,0 +1,9 @@
+
+#ifndef TEXT_UTILITY_H
+#define TEXT_UTILITY_H
+int wordCount(const ch`ar* str);
+int charCount(const char* str);
+void countVowelsAndConsonants(const char* str, int& vowels, int& consonants);
+void reverseString(char* str);
+
+#endif
