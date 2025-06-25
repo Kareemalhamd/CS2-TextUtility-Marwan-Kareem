e823071b8afc907edee1676368a678a055aec051
Kareem Mohamed <kareemalhamd@aucegypt.edu>
2025-06-25 18:04:22 +0300

    Update text-utility.h

--- text-utility.h
+++ text-utility.h
@@ -1,7 +1,7 @@
 
 #ifndef TEXT_UTILITY_H
 #define TEXT_UTILITY_H
-int wordCount(const ch`ar* str);
+int wordCount(const char* str);
 int charCount(const char* str);
 void countVowelsAndConsonants(const char* str, int& vowels, int& consonants);
 void reverseString(char* str);
