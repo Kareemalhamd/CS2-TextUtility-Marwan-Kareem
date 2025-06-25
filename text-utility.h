a4825d9a58b38eaf8c125d9cd113bc11b8a0009a
Marwan-AB <Marwan-Aboelnour@aucegypt.edu>
2025-06-25 19:59:35 +0300

    added last 4 function headers

--- text-utility.h
+++ text-utility.h
@@ -1,9 +1,12 @@
-
+#include<iostream>
 #ifndef TEXT_UTILITY_H
 #define TEXT_UTILITY_H
 int wordCount(const char* str);
 int charCount(const char* str);
 void countVowelsAndConsonants(const char* str, int& vowels, int& consonants);
 void reverseString(char* str);
-
+bool palindrome(const char* str);
+void convert_to_upper(char* str);
+void convert_to_lower(char* str);
+int substringcount(const char* str,const char* substr);
 #endif
